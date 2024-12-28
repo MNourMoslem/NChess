462928d54026a95cbd4a034529b5e66249d06bbe
= <normoslem256@gmail.com>
2024-12-28 23:38:32 +0300

    feat(nchess): including all needed header files to this file.

---
+++ NChess/src/nchess.h
@@ -0,0 +1,11 @@
+#ifndef NCHESS_SRC_NCHESS_H
+#define NCHESS_SRC_NCHESS_H
+
+#include "core.h"
+#include "board.h"
+#include "io.h"
+#include "bitboard.h"
+#include "types.h"
+#include "config.h"
+
+#endif

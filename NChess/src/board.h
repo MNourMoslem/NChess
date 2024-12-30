8a6808928b492a70898ffab32b281218571e364c
= <normoslem256@gmail.com>
2024-12-30 23:32:03 +0300

    feat(board): add en passant and game state flags to Board structure

--- NChess/src/board.h
+++ NChess/src/board.h
@@ -23,7 +23,15 @@ typedef struct
 {
     uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB];
     uint64 occupancy[NCH_SIDES_NB + 1];
+
+    uint64 castles;
     int flags;
+
+    int en_passant_idx;
+    uint64 en_passant_map;
+    uint64 en_passant_trg;
+
+    uint64 moves[NCH_SQUARE_NB];
 }Board;
 
 #define Board_WHITE_OCC(board) (board)->occupancy[NCH_White]
@@ -44,6 +52,48 @@ typedef struct
 #define Board_BLACK_QUEENS(board) (board)->bitboards[NCH_Black][NCH_Queen]
 #define Board_BLACK_KING(board) (board)->bitboards[NCH_Black][NCH_King]
 
+#define Board_PAWNMOVED (1 << 0)
+#define Board_ENPASSANT (1 << 1)
+#define Board_CAPTURE (1 << 2)
+#define Board_CHECK (1 << 3)
+#define Board_DOUBLECHECK (1 << 4)
+#define Board_CHECKMATE (1 << 5)
+#define Board_STALEMATE (1 << 6)
+#define Board_THREEFOLD (1 << 7)
+#define Board_FIFTYMOVES (1 << 8)
+#define Board_GAMEEND (1 << 9)
+#define Board_DRAW (1 << 10)
+#define Board_WIN (1 << 11)
+#define Board_TURN (1 << 12)
+
+#define Board_IS_PAWNMOVED(board) NCH_CHKFLG(board->flags, Board_PAWNMOVED)
+#define Board_IS_DOUBLECHECK(board) NCH_CHKFLG(board->flags, Board_DOUBLECHECK)
+#define Board_IS_ENPASSANT(board) NCH_CHKFLG(board->flags, Board_ENPASSANT)
+#define Board_IS_CAPTURE(board) NCH_CHKFLG(board->flags, Board_CAPTURE)
+#define Board_IS_CHECK(board) NCH_CHKFLG(board->flags, Board_CHECK)
+#define Board_IS_CHECKMATE(board) NCH_CHKFLG(board->flags, Board_CHECKMATE)
+#define Board_IS_STALEMATE(board) NCH_CHKFLG(board->flags, Board_STALEMATE)
+#define Board_IS_THREEFOLD(board) NCH_CHKFLG(board->flags, Board_THREEFOLD)
+#define Board_IS_FIFTYMOVES(board) NCH_CHKFLG(board->flags, Board_FIFTYMOVES)
+#define Board_IS_GAMEEND(board) NCH_CHKFLG(board->flags, Board_GAMEEND)
+#define Board_IS_DRAW(board) NCH_CHKFLG(board->flags, Board_DRAW)
+#define Board_IS_WHITEWIN(board) NCH_CHKFLG(board->flags, Board_WIN)
+#define Board_IS_BLACKWIN(board) !Board_IS_WHITEWIN(board)
+#define Board_IS_WHITETURN(board) NCH_CHKFLG(board->flags, Board_TURN)
+#define Board_IS_BLACKTURN(board) !Board_IS_WHITETURN(board)
+
+#define Board_GET_SIDE(board) (Board_IS_WHITETURN(board) ? NCH_White : NCH_Black)
+
+#define Board_CASTLE_WK 1
+#define Board_CASTLE_WQ 2
+#define Board_CASTLE_BK 4
+#define Board_CASTLE_BQ 8
+
+#define Board_IS_CASTLE_WK(board) NCH_CHKFLG(board->castles, Board_CASTLE_WK)
+#define Board_IS_CASTLE_WQ(board) NCH_CHKFLG(board->castles, Board_CASTLE_WQ)
+#define Board_IS_CASTLE_BK(board) NCH_CHKFLG(board->castles, Board_CASTLE_BK)
+#define Board_IS_CASTLE_BQ(board) NCH_CHKFLG(board->castles, Board_CASTLE_BQ)
+
 void
 Board_Init(Board* board);
 
@@ -55,5 +105,8 @@ Board_SetSquare(Board* board, Side side, Piece ptype, int sqr_idx);
 
 void
 Board_SetBitboard(Board* board, Side side, Piece ptype, uint64 bb);
+
+int
+Board_IsCheck(Board* board);
 
 #endif

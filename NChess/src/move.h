b572eea782afcd0dd5fbce85844d5fb4e7543190
= <normoslem256@gmail.com>
2025-01-02 23:51:15 +0300

    feat(move): add move undoing, saving, and parsing functions

--- NChess/src/move.h
+++ NChess/src/move.h
@@ -6,12 +6,21 @@
 #include "config.h"
 #include "board.h"
 
-void
-make_move(Board* board, Square from_, Square to_, Piece promotion, uint8 castle);
-
 NCH_STATIC_INLINE int
 is_valid_move(Board* board, Square from_, Square to_){
     return NCH_CHKFLG(board->moves[from_], NCH_SQR(to_));
 }
+
+uint32
+make_move(Board* board, Square from_, Square to_, Piece promotion, uint8 castle);
+
+void
+undo_move(Board* board, Side side, uint32 move);
+
+uint32
+save_move(Square from_, Square to_, uint8 castle, Piece promotion, int is_promotion, int is_enpassant, Piece captured_piece);
+
+void
+parse_move(uint32 move, Square* from_, Square* to_, uint8* castle, Piece* promotion, int* is_promotion, int* is_enpassant, Piece* captured_piece);
 
 #endif //NCHESS_SRC_MOVE_H

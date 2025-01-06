400e947f379b45dd227ce67e4d100a8088f1281c
= <normoslem256@gmail.com>
2025-01-07 02:11:25 +0300

    feat(move): refactor move handling by introducing Move type and updating related functions

--- NChess/src/move.c
+++ NChess/src/move.c
@@ -2,36 +2,119 @@
 #include "board.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "utils.h"
 
-uint32
-save_move(Square from_, Square to_, uint8 castle, Piece promotion, int is_promotion, int is_enpassant, Piece captured_piece){
-    uint32 move = 0u;
-    move |= from_;
-    move |= to_ << 6;
-    move |= castle << 12;
-    move |= promotion << 16;
-    move |= is_promotion << 20;
-    move |= is_enpassant << 21;
-    move |= captured_piece << 22;
-    return move;
+NCH_STATIC_INLINE int
+is_valid_column(char arg){
+    return arg >= 'a' && arg <= 'h';
+}
+
+NCH_STATIC_INLINE int
+is_valid_row(char arg){
+    return arg >= '1' && arg <= '8';
 }
 
 void
-parse_move(uint32 move, Square* from_, Square* to_, uint8* castle, Piece* promotion, int* is_promotion, int* is_enpassant, Piece* captured_piece){
-    *from_ = move & 0x3F;
-    *to_ = (move >> 6) & 0x3F;
-    *castle = (move >> 12) & 0xF;
-    *promotion = (move >> 16) & 0xF;
-    *is_promotion = (move >> 20) & 0x1;
-    *is_enpassant = (move >> 21) & 0x1;
-    *captured_piece = (move >> 22) & 0xF;
+Move_Parse(Move move, Square* from_, Square* to_, uint8* castle, Piece* promotion){
+    *from_ = Move_FROM(move);
+    *to_ = Move_TO(move);
+    *castle = Move_CASTLE(move);
+    *promotion = Move_PRO_PIECE(move);
 }
 
-NCH_STATIC_INLINE void
-reset_enpassant_variable(Board* board){
-    board->en_passant_idx = 0;
-    board->en_passant_map = 0ull;
-    board->en_passant_trg = 0ull;
+Move
+Move_New(Square from_, Square to_, uint8 castle, Piece promotion){
+    Move move;
+    move = Move_ASSIGN_FROM(from_) | Move_ASSIGN_TO(to_) 
+         | Move_ASSIGN_CASTLE(castle) | Move_ASSIGN_PRO_PIECE(promotion) ;
+    return move;
+}
+
+int
+Move_ParseFromString(Board* board, char* arg, Square* from_, Square* to_, Piece* promotion, uint8* castle){
+    int len = strlen(arg);
+    if (len > 5 || len < 4){
+        return -1;
+    }
+
+    if (!is_valid_column(arg[0]) || !is_valid_row(arg[1])
+         || !is_valid_column(arg[2]) || !is_valid_row(arg[3]))
+    {
+        return -1;
+    }
+
+    *from_ = ('h' - arg[0]) + 8 * (arg[1] - '1');
+    *to_ = ('h' - arg[2]) + 8 * (arg[3] - '1');
+
+    if (len == 5){
+        if (arg[4] == 'q'){
+            *promotion = NCH_Queen;
+        }
+        else if (arg[4] == 'r'){
+            *promotion = NCH_Rook;
+        }
+        else if (arg[4] == 'b'){
+            *promotion = NCH_Bishop;
+        }
+        else if (arg[4] == 'n'){
+            *promotion = NCH_Knight;
+        }
+        else{
+            return -1;
+        }
+    }
+    else{
+        *promotion = 0;
+    }
+    
+    *castle = 0;
+    if (Board_IS_WHITETURN(board)){
+        if (*from_ == NCH_E1 && Board_WHITE_KING(board) == NCH_SQR(NCH_E1)){
+            if (*to_ == NCH_G1){
+                *castle |= Board_CASTLE_WK;
+            }
+            else if (*to_ == NCH_C1){
+                *castle |= Board_CASTLE_WQ;
+            }
+        }
+    }
+    else{
+        if (*from_ == NCH_E8 && Board_BLACK_KING(board) == NCH_SQR(NCH_E8)){
+            if (*to_ == NCH_G8){
+                *castle = Board_CASTLE_BK;
+            }
+            else if (*to_ == NCH_C8){
+                *castle = Board_CASTLE_BQ;
+            }
+        }
+    }
+
+    return 0;
+}
+
+Move
+Move_FromString(Board* board, char* move){
+    Square from_, to_;
+    Piece promotion;
+    uint8 castle;
+    Move_ParseFromString(board, move, &from_, &to_, &promotion, &castle);
+    return Move_New(from_, to_, castle, promotion);
+}
+
+void
+Move_Print(Move move){
+    Square from_, to_;
+    Piece promotion;
+    uint8 castle;
+
+    Move_Parse(move, &from_, &to_, &castle, &promotion);
+
+    printf("from: %i\n", from_);
+    printf("to: %i\n", to_);
+    printf("castle: %s\n", NCH_CHKFLG(castle, Board_CASTLE_WK | Board_CASTLE_BK) ? "king"
+                        : NCH_CHKFLG(castle, Board_CASTLE_WQ | Board_CASTLE_WQ) ? "queen" : "none");
+    printf("promotion piece: %i\n", promotion);
 }
 
 NCH_STATIC_INLINE void*
@@ -43,44 +126,23 @@ get_target_map(Board* board, Side side, Square sqr){
 
 NCH_STATIC_INLINE void
 set_piece(Board* board, Side side, Square sqr, Piece piece){
-    NCH_SETFLG(board->bitboards[side][piece], NCH_SQR(sqr));
+    board->bitboards[side][piece] |= NCH_SQR(sqr);
     board->piecetables[side][sqr] = piece;
-    NCH_SETFLG(board->occupancy[side], NCH_SQR(sqr));
-    Board_ALL_OCC(board) = Board_WHITE_OCC(board) | Board_BLACK_OCC(board);
 }
 
 NCH_STATIC_INLINE void
 remove_piece(Board* board, Side side, Square sqr){
     uint64* piece_map = get_target_map(board, side, sqr);
-    NCH_RMVFLG(*piece_map, NCH_SQR(sqr));
-    board->piecetables[side][sqr] = NCH_NO_PIECE;
-    NCH_RMVFLG(board->occupancy[side], NCH_SQR(sqr));
-    Board_ALL_OCC(board) = Board_WHITE_OCC(board) | Board_BLACK_OCC(board);
+    if (piece_map){
+        *piece_map &= ~NCH_SQR(sqr);
+        board->piecetables[side][sqr] = NCH_NO_PIECE;
+    }
 }
 
 NCH_STATIC_INLINE void
 move_piece(Board* board, Side side, Square from_, Square to_){
-    uint64* piece_map = get_target_map(board, side, from_);
-    NCH_RMVFLG(*piece_map, NCH_SQR(from_));
-    NCH_SETFLG(*piece_map, NCH_SQR(to_));
-
-    board->piecetables[side][to_] = board->piecetables[side][from_];
-    board->piecetables[side][from_] = NCH_NO_PIECE;
-    
-    NCH_RMVFLG(board->occupancy[side], NCH_SQR(from_));
-    NCH_SETFLG(board->occupancy[side], NCH_SQR(to_));
-    Board_ALL_OCC(board) = Board_WHITE_OCC(board) | Board_BLACK_OCC(board);
-}
-
-NCH_STATIC_INLINE Piece
-capture_piece_if_possible(Board* board, Side trg_side, Square sqr){
-    if (board->piecetables[trg_side][sqr] != NCH_NO_PIECE){
-        Piece captured_piece = board->piecetables[trg_side][sqr];
-        remove_piece(board, trg_side, sqr);
-        NCH_SETFLG(board->flags, Board_CAPTURE);
-        return captured_piece;
-    }
-    return NCH_NO_PIECE;
+    set_piece(board, side, to_, board->piecetables[side][from_]);
+    remove_piece(board, side, from_);
 }
 
 void
@@ -91,6 +153,7 @@ make_promotion(Board* board, Side side, uint64 sqr, Piece promotion){
 
     remove_piece(board, side, sqr);
     set_piece(board, side, sqr, promotion);
+    NCH_SETFLG(board->flags, Board_PROMOTION);
 }
 
 NCH_STATIC_INLINE int
@@ -98,28 +161,32 @@ is_pawn_move(Board* board, Side side, Square from_){
     return board->piecetables[side][from_] == NCH_Pawn;
 }
 
+NCH_STATIC_INLINE Piece
+capture_piece_if_possible(Board* board, Side trg_side, Square sqr){
+    uint64* piece_map = get_target_map(board, trg_side, sqr);
+    if (piece_map){
+        *piece_map &= ~NCH_SQR(sqr);
+        Piece captured_piece = board->piecetables[trg_side][sqr];
+        board->piecetables[trg_side][sqr] = NCH_NO_PIECE;
+        NCH_SETFLG(board->flags, Board_CAPTURE);
+        return captured_piece;
+    }    
+    return NCH_NO_PIECE;
+}
+
 NCH_STATIC_INLINE Piece 
-play_pawn_move(Board* board, Side side, Square from_, Square to_, Piece promotion, int* is_promotion, int* is_enpassant){
+play_pawn_move(Board* board, Side side, Square from_, Square to_, Piece promotion){
     move_piece(board, side, from_, to_);
     if (NCH_SQR(to_) == board->en_passant_trg){
        to_ = side == NCH_White ? to_ - 8 : to_ + 8; 
-       *is_enpassant = 1;
-    }
-    else{
-        *is_enpassant = 0;
+        NCH_SETFLG(board->flags, Board_ENPASSANT);
     }
 
-    Side trg_side = side == NCH_White ? NCH_Black : NCH_White;
-    Piece captured_piece = capture_piece_if_possible(board, trg_side, to_);
+    Piece captured_piece = capture_piece_if_possible(board, TARGET_SIDE(side), to_);
     NCH_SETFLG(board->flags, Board_PAWNMOVED);
 
     if (to_ - from_ == 16 || from_ - to_ == 16){
-        board->en_passant_idx = to_;
-        board->en_passant_map = NCH_SQR(to_) | (((NCH_NXTSQR_RIGHT(NCH_SQR(to_)) & 0x7f7f7f7f7f7f7f7f)
-                                                |(NCH_NXTSQR_LEFT(NCH_SQR(to_)) & 0xfefefefefefefefe))
-                                                & board->bitboards[trg_side][NCH_Pawn]);
-        board->en_passant_trg = side == NCH_White ? NCH_NXTSQR_DOWN(NCH_SQR(to_))
-                                                  : NCH_NXTSQR_UP(NCH_SQR(to_));
+        set_board_enp_settings(board, side, to_);
     }
     else{
         reset_enpassant_variable(board);
@@ -127,73 +194,60 @@ play_pawn_move(Board* board, Side side, Square from_, Square to_, Piece promotio
 
     if (to_ <= NCH_A1 || to_ >= NCH_H8){
         make_promotion(board, side, to_, promotion);
-        *is_promotion = 1;
     }
-    else{
-        *is_promotion = 0;
-    }
-
     return captured_piece;
 }
 
 NCH_STATIC Piece
-play_move(Board* board, Side side, Square from_, Square to_, Piece promotion, int* is_promotion, int* is_enpassant){
+play_move(Board* board, Side side, Square from_, Square to_, Piece promotion){
     if (is_pawn_move(board, side, from_)){
-        return play_pawn_move(board, side, from_, to_, promotion, is_promotion, is_enpassant);
+        return play_pawn_move(board, side, from_, to_, promotion);
     }
-    *is_promotion = 0;
-    *is_enpassant = 0;
 
-    Side trg_side = side == NCH_White ? NCH_Black : NCH_White;
     move_piece(board, side, from_, to_);
-    Piece captured_piece = capture_piece_if_possible(board, trg_side, to_);
+    Piece captured_piece = capture_piece_if_possible(board, TARGET_SIDE(side), to_);
     reset_enpassant_variable(board);
     return captured_piece;
 }
 
 void
 play_castle_move(Board* board, Side side, Square from_, Square to_, int king_side){
+    move_piece(board, side, from_, to_);
     if (king_side){
-        move_piece(board, side, from_, to_);
         move_piece(board, side, from_ - 3, to_ + 1);
     }
     else{
-        move_piece(board, side, from_, to_);
         move_piece(board, side, from_ + 4, to_ - 1);
     }
     reset_enpassant_variable(board);
 }
 
-uint32
-make_move(Board* board, Square from_, Square to_, Piece promotion, uint8 castle){    
+Piece
+make_move(Board* board, Square from_, Square to_, Piece promotion, uint8 castle){
     Piece captured_piece;
-    int is_promotion, is_enpassant;
     if (castle){
-        play_castle_move(board, Board_GET_SIDE(board), from_, to_, NCH_CHKUNI(castle, Board_CASTLE_WK | Board_CASTLE_BK));
+        play_castle_move(board, Board_GET_SIDE(board), from_, to_,
+                         NCH_CHKUNI(castle, Board_CASTLE_WK | Board_CASTLE_BK));
         captured_piece = NCH_NO_PIECE;
-        is_promotion = 0;
-        is_enpassant = 0;
     }
     else{
-        captured_piece = play_move(board, Board_GET_SIDE(board), from_, to_, promotion, &is_promotion, &is_enpassant);
+        captured_piece = play_move(board, Board_GET_SIDE(board), from_, to_, promotion);
     }
-    return save_move(from_, to_, castle, promotion, is_promotion, is_enpassant, captured_piece);
-}   
+    set_board_occupancy(board);
+    return captured_piece;
+}
 
 void
-undo_move(Board* board, Side side, uint32 move){
+undo_move(Board* board, Side side, Move move, int is_enpassant,
+         int is_promotion, Piece last_captured_piece){
     Square from_;
     Square to_;
     uint8 castle;
     Piece promotion;
-    int is_promotion;
-    int is_enpassant;
-    Piece captured_piece;
-    parse_move(move, &from_, &to_, &castle, &promotion, &is_promotion, &is_enpassant, &captured_piece);
-
+    Move_Parse(move, &from_, &to_, &castle, &promotion);
+    
     if (castle){
-        int king_side = NCH_CHKUNI(castle, Board_CASTLE_WK | Board_CASTLE_BK);
-        if (king_side){
+        if (NCH_CHKUNI(castle, Board_CASTLE_WK | Board_CASTLE_BK)){
             move_piece(board, side, to_, from_);
             move_piece(board, side, to_ + 1, from_ - 3);
         }
@@ -202,19 +256,23 @@ undo_move(Board* board, Side side, uint32 move){
             move_piece(board, side,  to_ - 1, from_ + 4);
         }
     }
-    else if (is_promotion){
-        remove_piece(board, side, to_);
-        set_piece(board, side, from_, NCH_Pawn);
-    }
     else{
-        move_piece(board, side, to_, from_);
+        if (is_promotion){
+            remove_piece(board, side, to_);
+            set_piece(board, side, from_, NCH_Pawn);
+        }
+        else{
+            move_piece(board, side, to_, from_);
+        }
+        
+        if (is_enpassant){
+            set_piece(board, TARGET_SIDE(side), side == NCH_White ? to_ - 8 : to_ + 8, NCH_Pawn);
+        }
+        else if(last_captured_piece != NCH_NO_PIECE){
+            set_piece(board, TARGET_SIDE(side), to_, last_captured_piece);
+        }
     }
-    
-    if (is_enpassant){
-        printf("it is enpassant!!!!!\n");
-        set_piece(board, side == NCH_White ? NCH_Black : NCH_White, side == NCH_White ? to_ - 8 : to_ + 8, captured_piece);
-    }
-    else if(captured_piece != NCH_NO_PIECE){
-        set_piece(board, side == NCH_White ? NCH_Black : NCH_White, to_, captured_piece);
-    }
+
+
+    set_board_occupancy(board);
 }
--- NChess/src/move.h
+++ NChess/src/move.h
@@ -7,20 +7,30 @@
 #include "board.h"
 
 NCH_STATIC_INLINE int
-is_valid_move(Board* board, Square from_, Square to_){
+Move_IsValid(Board* board, Square from_, Square to_){
     return NCH_CHKFLG(board->moves[from_], NCH_SQR(to_));
 }
 
-uint32
+Piece
 make_move(Board* board, Square from_, Square to_, Piece promotion, uint8 castle);
 
 void
-undo_move(Board* board, Side side, uint32 move);
+undo_move(Board* board, Side side, Move move, int is_enpassant,
+         int is_promotion, Piece last_captured_piece);
 
-uint32
-save_move(Square from_, Square to_, uint8 castle, Piece promotion, int is_promotion, int is_enpassant, Piece captured_piece);
+Move
+Move_New(Square from_, Square to_, uint8 castle, Piece promotion);
 
 void
-parse_move(uint32 move, Square* from_, Square* to_, uint8* castle, Piece* promotion, int* is_promotion, int* is_enpassant, Piece* captured_piece);
+Move_Parse(Move move, Square* from_, Square* to_, uint8* castle, Piece* promotion);
+
+int
+Move_ParseFromString(Board* board, char* arg, Square* from_, Square* to_, Piece* promotion, uint8* castle);
+
+Move
+Move_FromString(Board* board, char* move);
+
+void
+Move_Print(Move move);
 
 #endif //NCHESS_SRC_MOVE_H

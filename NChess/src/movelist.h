2c37f29783b436bd2d0ccae400e9f3d56ca42c0e
= <normoslem256@gmail.com>
2025-01-07 02:11:53 +0300

    feat(movelist): enhance MoveNode structure with gamestate and additional flags for improved move handling

--- NChess/src/movelist.h
+++ NChess/src/movelist.h
@@ -3,10 +3,16 @@
 
 #include "types.h"
 #include "config.h"
+#include "core.h"
 
 typedef struct MoveNode
 {
-    uint32 move;
+    Move move;
+    uint32 gamestate;
+    int gameflags;
+    int fifty_count;
+    uint8 castle;
+
     struct MoveNode* prev;
     struct MoveNode* next;
 }MoveNode;
@@ -18,12 +24,13 @@ typedef struct
     int len;
 }MoveList;
 
-
 void*
 MoveList_New();
 
 int
-MoveList_Append(MoveList* movelist, uint32 move);
+MoveList_Append(MoveList* movelist, Move move, int is_enpassant,
+                     int is_promotion, Square enp_sqr, Piece captured_piece,
+                     int fifty_count, uint8 castle_flags, int flags);
 
 void
 MoveList_Pop(MoveList* movelist);
@@ -31,12 +38,24 @@ MoveList_Pop(MoveList* movelist);
 void
 MoveList_Free(MoveList* movelist);
 
-NCH_STATIC_INLINE uint32
-MoveList_LastMove(MoveList* movelist){
+NCH_STATIC_INLINE MoveNode*
+MoveList_Last(MoveList* movelist){
     if (movelist->len > 0){
-        return movelist->last->move;
+        return movelist->last;
     }
     return 0;
 }
+
+#define MoveNode_FROM(node) Move_FROM((node)->move)
+#define MoveNode_TO(node) Move_TO((node)->move)
+#define MoveNode_CASTLE(node) Move_CASTLE((node)->move)
+#define MoveNode_PRO_PIECE(node) Move_PRO_PIECE((node)->move)
+#define MoveNode_ENPASSANT(node) ((node)->gamestate & 0x1)
+#define MoveNode_PROMOTION(node) (((node)->gamestate >> 1) & 0x1)
+#define MoveNode_ENP_SQR(node) (((node)->gamestate >> 2) & 0x3F)
+#define MoveNode_CAP_PIECE(node) (((node)->gamestate >> 8) & 0xF)
+#define MoveNode_FIFTY_COUNT(node) ((node)->fifty_count)
+#define MoveNode_CASTLE_FLAGS(node) ((node)->castle)
+#define MoveNode_GAME_FLAGS(node) ((node)->gameflags)
 
 #endif

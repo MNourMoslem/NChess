9e46a5d5ee12991cb66d2cfc739b461a9438b744
= <normoslem256@gmail.com>
2025-01-06 18:32:01 +0300

    feat(hash): refactor BoardDict functions to use bitboards instead of Board struct

--- NChess/src/hash.c
+++ NChess/src/hash.c
@@ -4,45 +4,45 @@
 #include <stdio.h>
 
 NCH_STATIC_INLINE int
-board_to_key(Board* board){
+board_to_key(uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB]){
     uint64 maps = 0ull;
-    maps = ~Board_WHITE_PAWNS(board)
-         ^ ~Board_WHITE_KNIGHTS(board)
-         ^ ~Board_WHITE_BISHOPS(board)
-         ^ ~Board_WHITE_ROOKS(board)
-         ^ ~Board_WHITE_QUEENS(board)
-         ^ ~Board_WHITE_KING(board)
-         ^ ~Board_BLACK_PAWNS(board)
-         ^ ~Board_BLACK_KNIGHTS(board)
-         ^ ~Board_BLACK_BISHOPS(board)
-         ^ ~Board_BLACK_ROOKS(board)
-         ^ ~Board_BLACK_QUEENS(board)
-         ^ ~Board_BLACK_KING(board);
+    maps = ~bitboards[NCH_White][NCH_Pawn]
+         ^ ~bitboards[NCH_White][NCH_Knight]
+         ^ ~bitboards[NCH_White][NCH_Bishop]
+         ^ ~bitboards[NCH_White][NCH_Rook]
+         ^ ~bitboards[NCH_White][NCH_Queen]
+         ^ ~bitboards[NCH_White][NCH_King]
+         ^ ~bitboards[NCH_Black][NCH_Pawn]
+         ^ ~bitboards[NCH_Black][NCH_Knight]
+         ^ ~bitboards[NCH_Black][NCH_Bishop]
+         ^ ~bitboards[NCH_Black][NCH_Rook]
+         ^ ~bitboards[NCH_Black][NCH_Queen]
+         ^ ~bitboards[NCH_Black][NCH_King];
 
     return (int)((((maps) >> 32) ^ maps) & 0x000000007FFFFFFF);
 }
 
 NCH_STATIC_INLINE int
-get_idx(Board* board){
-    return board_to_key(board) % NCH_BOARD_DICT_SIZE;
+get_idx(uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB]){
+    return board_to_key(bitboards) % NCH_BOARD_DICT_SIZE;
 }
 
 NCH_STATIC_INLINE int
-is_same_board(Board* board, BoardNode* node){
-    return (0 == memcmp(board->bitboards[NCH_White], node->bitboards[NCH_White], sizeof(node->bitboards[NCH_White])))
-        && (0 == memcmp(board->bitboards[NCH_Black], node->bitboards[NCH_Black], sizeof(node->bitboards[NCH_Black])));
+is_same_board(uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB], BoardNode* node){
+    return (0 == memcmp(bitboards[NCH_White], node->bitboards[NCH_White], sizeof(node->bitboards[NCH_White])))
+        && (0 == memcmp(bitboards[NCH_Black], node->bitboards[NCH_Black], sizeof(node->bitboards[NCH_Black])));
 }
 
 NCH_STATIC_INLINE void
-set_bitboards(Board* board, BoardNode* node){
-    memcpy(node->bitboards[NCH_White], board->bitboards[NCH_White], sizeof(node->bitboards[NCH_White]));
-    memcpy(node->bitboards[NCH_Black], board->bitboards[NCH_Black], sizeof(node->bitboards[NCH_Black]));
+set_bitboards(uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB], BoardNode* node){
+    memcpy(node->bitboards[NCH_White], bitboards[NCH_White], sizeof(node->bitboards[NCH_White]));
+    memcpy(node->bitboards[NCH_Black], bitboards[NCH_Black], sizeof(node->bitboards[NCH_Black]));
 }
 
 NCH_STATIC int
-set_node(Board* board, BoardNode* node){
-    if (node->empty || is_same_board(board, node)){
-        set_bitboards(board, node);
+set_node(uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB], BoardNode* node){
+    if (node->empty || is_same_board(bitboards, node)){
+        set_bitboards(bitboards, node);
         if (node->empty){
             node->count = 1;
             node->empty = 0;
@@ -54,7 +54,7 @@ set_node(Board* board, BoardNode* node){
     }
     else{
         if (node->next){
-            return set_node(board, node->next);
+            return set_node(bitboards, node->next);
         }
         else{
             BoardNode* newnode = malloc(sizeof(BoardNode));
@@ -62,7 +62,7 @@ set_node(Board* board, BoardNode* node){
                 return -1;
             }
 
-            set_bitboards(board, newnode);
+            set_bitboards(bitboards, newnode);
             newnode->empty = 0;
             newnode->count = 1;
             newnode->next = NULL;
@@ -73,13 +73,13 @@ set_node(Board* board, BoardNode* node){
 }
 
 NCH_STATIC BoardNode*
-get_node(Board* board, BoardNode* node){
-    if (is_same_board(board, node) && !node->empty){
+get_node(uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB], BoardNode* node){
+    if (!node->empty && is_same_board(bitboards, node)){
         return node;
     }
     else{
         if (node->next){
-            return get_node(board, node->next);
+            return get_node(bitboards, node->next);
         }
         return NULL;
     }
@@ -120,9 +120,9 @@ BoardDict_Free(BoardDict* dict){
 }
 
 int
-BoardDict_GetCount(BoardDict* dict, Board* board){
-    int idx = get_idx(board);
-    BoardNode* node = get_node(board, &dict->nodes[idx]);
+BoardDict_GetCount(BoardDict* dict, uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB]){
+    int idx = get_idx(bitboards);
+    BoardNode* node = get_node(bitboards, &dict->nodes[idx]);
     if (!node){
         return -1;
     }
@@ -130,21 +130,20 @@ BoardDict_GetCount(BoardDict* dict, Board* board){
 }
 
 int
-BoardDict_Add(BoardDict* dict, Board* board){
-    int idx = get_idx(board);
-    return set_node(board, &dict->nodes[idx]);
+BoardDict_Add(BoardDict* dict, uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB]){
+    int idx = get_idx(bitboards);
+    return set_node(bitboards, &dict->nodes[idx]);
 }
 
 int
-BoardDict_Remove(BoardDict* dict, Board* board){
-    int idx = get_idx(board);
-    BoardNode* node = get_node(board, &dict->nodes[idx]);
+BoardDict_Remove(BoardDict* dict, uint64 bitboards[NCH_SIDES_NB][NCH_PIECE_NB]){
+    int idx = get_idx(bitboards);
+    BoardNode* node = get_node(bitboards, &dict->nodes[idx]);
     if (!node){
         return -1;
     }
 
     if (node->count > 1){
-        printf("BoardDict_Remove count dicreased\n");
         node->count -= 1;
     }
     else{
@@ -152,12 +151,11 @@ BoardDict_Remove(BoardDict* dict, Board* board){
         if (prev == node){
             if (!prev->next){
                 prev->empty = 1;
-                printf("BoardDict_Remove made it empty\n");
             }
             else{
-                *prev = *(prev->next);
-                printf("BoardDict_Remove prev\n");
-                free(prev->next);
+                BoardNode* temp = prev->next;
+                *prev = *temp;
+                free(temp);
             }
         }
         else{
@@ -167,12 +165,9 @@ BoardDict_Remove(BoardDict* dict, Board* board){
                     prev = prev->next;
                 }
                 else{
-                    printf("BoardDict_Remove found nothing\n");
                     return -1;
                 }
             }
-            
-            printf("BoardDict_Remove removed and relinked\n");
             prev->next = prev->next->next;
             free(node);
         }

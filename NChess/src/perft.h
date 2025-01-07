a165843592949254824330523f996dc069fac711
= <normoslem256@gmail.com>
2025-01-07 23:29:03 +0300

    feat(perft): add header file for performance testing functions

---
+++ NChess/src/perft.h
@@ -0,0 +1,12 @@
+#ifndef NCHESS_SRC_PERFT_H
+#define NCHESS_SRC_PERFT_H
+
+#include "board.h"
+
+long long
+Board_Perft(Board* board, int depth);
+
+long long
+Board_PerftNoPrint(Board* board, int depth);
+
+#endif

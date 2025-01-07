7c2361be5509feb6a904247205813228327b2abb
= <normoslem256@gmail.com>
2025-01-07 23:26:48 +0300

    feat(loops): add named loop macro for improved readability and usability

--- NChess/src/loops.h
+++ NChess/src/loops.h
@@ -9,4 +9,8 @@
 idx = NCH_SQRIDX(bitmap);\
 for (uint64 map = bitmap; map != 0; NCH_RMVFLG(map, NCH_SQR(idx)), idx = NCH_SQRIDX(map))
 
+#define LOOP_U64_NAMED(bb_name, idx_name, bb)\
+idx_name = NCH_SQRIDX(bb);\
+for (uint64 bb_name = bb; bb_name != 0; NCH_RMVFLG(bb_name, NCH_SQR(idx_name)), idx_name = NCH_SQRIDX(bb_name))
+
 #endif

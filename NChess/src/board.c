443d005e13b3a58a50a3355b7fbd5070691dc807
= <normoslem256@gmail.com>
2025-01-07 23:26:02 +0300

    feat(board): implement function to retrieve legal moves for the current board state

--- NChess/src/board.c
+++ NChess/src/board.c
@@ -390,3 +390,84 @@ Board_NMoves(Board* board){
 
     return count;
 }
+
+int
+Board_GetLegalMoves(Board* board, Move* moves){
+    uint64 passer_pawns = Board_IS_WHITETURN(board) ?
+                        Board_WHITE_PAWNS(board) & NCH_ROW7 :
+                        Board_BLACK_PAWNS(board) & NCH_ROW2 ;
+
+    uint64 king_sqr = Board_IS_WHITETURN(board) ? Board_WHITE_KING(board)
+                                                : Board_BLACK_KING(board);
+    int temp = NCH_SQRIDX(king_sqr);
+    uint64 castle_moves = board->moves[temp] 
+                        & (NCH_SQR(NCH_G1) | NCH_SQR(NCH_C1)
+                        | NCH_SQR(NCH_G8) | NCH_SQR(NCH_C8));
+    board->moves[temp] &= ~castle_moves;
+
+    uint64 occ_map = (Board_IS_WHITETURN(board) ? Board_WHITE_OCC(board)
+                                               : Board_BLACK_OCC(board))
+                    & ~passer_pawns;
+
+
+    int idx, counter = 0;
+    LOOP_U64_NAMED(occ, temp, occ_map){
+        if (board->moves[temp]){
+            LOOP_U64_NAMED(mv, idx, board->moves[temp]){
+                moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(idx);
+            }
+        }
+    }
+    
+    LOOP_U64_NAMED(occ, temp, passer_pawns){
+        if (board->moves[temp]){
+            LOOP_U64_NAMED(mv, idx, board->moves[temp]){
+                moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(idx) 
+                                | Move_ASSIGN_PRO_PIECE(NCH_Queen);
+
+                moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(idx) 
+                                | Move_ASSIGN_PRO_PIECE(NCH_Rook);
+
+                moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(idx) 
+                                | Move_ASSIGN_PRO_PIECE(NCH_Bishop);
+
+                moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(idx) 
+                                | Move_ASSIGN_PRO_PIECE(NCH_Knight);
+            }
+        }
+    }
+
+    temp = NCH_SQRIDX(king_sqr);
+    board->moves[temp] |= castle_moves;
+
+    if (NCH_CHKFLG(NCH_SQR(NCH_E1) | NCH_SQR(NCH_E8), king_sqr) && castle_moves){
+        if (NCH_CHKUNI(castle_moves, (NCH_SQR(NCH_G1) | NCH_SQR(NCH_G8)))){
+            moves[counter++] = Move_ASSIGN_FROM(temp) 
+                            | Move_ASSIGN_TO(NCH_SQRIDX(castle_moves & (NCH_SQR(NCH_G1) | NCH_SQR(NCH_G8)))) 
+                            | Move_ASSIGN_CASTLE(
+                                castle_moves & (NCH_SQR(NCH_G1) | NCH_SQR(NCH_G8)) ?
+                                (Board_CASTLE_WK | Board_CASTLE_BK) : 0
+                            );
+        }
+
+        if (NCH_CHKUNI(castle_moves, (NCH_SQR(NCH_C1) | NCH_SQR(NCH_C8)))){
+            moves[counter++] = Move_ASSIGN_FROM(temp) 
+                            | Move_ASSIGN_TO(NCH_SQRIDX(castle_moves & (NCH_SQR(NCH_C1) | NCH_SQR(NCH_C8)))) 
+                            | Move_ASSIGN_CASTLE(
+                                castle_moves & (NCH_SQR(NCH_C1) | NCH_SQR(NCH_C8)) ?
+                                (Board_CASTLE_WQ | Board_CASTLE_BQ) : 0
+                            );
+        }
+    }
+    else{
+        if (castle_moves){
+            moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(NCH_SQRIDX(castle_moves));
+            castle_moves &= castle_moves-1;
+        }
+        if (castle_moves){
+            moves[counter++] = Move_ASSIGN_FROM(temp) | Move_ASSIGN_TO(NCH_SQRIDX(castle_moves));
+        }
+    }
+
+    return counter;
+}

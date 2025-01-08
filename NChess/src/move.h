da0e07361b1504f22427f92a859304c4f9213331
= <normoslem256@gmail.com>
2025-01-08 23:56:34 +0300

    refactor(move): remove board dependency and simplify move parsing functions

--- NChess/src/move.h
+++ NChess/src/move.h
@@ -4,19 +4,6 @@
 #include "core.h"
 #include "types.h"
 #include "config.h"
-#include "board.h"
-
-NCH_STATIC_INLINE int
-Move_IsValid(Board* board, Square from_, Square to_){
-    return NCH_CHKFLG(board->moves[from_], NCH_SQR(to_));
-}
-
-Piece
-make_move(Board* board, Square from_, Square to_, Piece promotion, uint8 castle);
-
-void
-undo_move(Board* board, Side side, Move move, int is_enpassant,
-         int is_promotion, Piece last_captured_piece);
 
 Move
 Move_New(Square from_, Square to_, uint8 castle, Piece promotion);
@@ -25,18 +12,15 @@ void
 Move_Parse(Move move, Square* from_, Square* to_, uint8* castle, Piece* promotion);
 
 int
-Move_ParseFromString(Board* board, char* arg, Square* from_, Square* to_, Piece* promotion, uint8* castle);
+Move_ParseFromString(char* arg, Square* from_, Square* to_, Piece* promotion, uint8* castle);
 
 Move
-Move_FromString(Board* board, char* move);
+Move_FromString(char* move);
 
 void
 Move_Print(Move move);
 
 void
-Move_AsString(Move move, Piece promotion, char* dst);
-
-void
-Move_AsStringBoard(Board* board, Move move, char* dst);
+Move_AsString(Move move, char* dst);
 
 #endif //NCHESS_SRC_MOVE_H

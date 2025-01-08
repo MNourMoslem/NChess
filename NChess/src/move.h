67adf411c8156b0b5b47b5250f8ea041a1246538
= <normoslem256@gmail.com>
2025-01-09 00:14:51 +0300

    feat(move): introduce Move type and macros for efficient move handling

--- NChess/src/move.h
+++ NChess/src/move.h
@@ -5,6 +5,8 @@
 #include "types.h"
 #include "config.h"
 
+typedef uint32 Move;
+
 Move
 Move_New(Square from_, Square to_, uint8 castle, Piece promotion);
 
@@ -22,5 +24,15 @@ Move_Print(Move move);
 
 void
 Move_AsString(Move move, char* dst);
+
+#define Move_ASSIGN_FROM(from_) ((from_))
+#define Move_ASSIGN_TO(to_) ((to_) << 6)
+#define Move_ASSIGN_CASTLE(castle) ((castle) << 12)
+#define Move_ASSIGN_PRO_PIECE(pro_piece) ((pro_piece) << 16)
+
+#define Move_FROM(move) ((move) & 0x3F)
+#define Move_TO(move) (((move) >> 6) & 0x3F)
+#define Move_CASTLE(move) (((move) >> 12) & 0xF)
+#define Move_PRO_PIECE(move) (((move) >> 16) & 0xF)
 
 #endif //NCHESS_SRC_MOVE_H

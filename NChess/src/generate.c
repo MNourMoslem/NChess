a51d1781673bbbc70da1e45515d13d5f0723dcb5
= <normoslem256@gmail.com>
2025-01-09 00:35:47 +0300

    refactor(generate): rename piece generation functions for consistency and clarity

--- NChess/src/generate.c
+++ NChess/src/generate.c
@@ -5,41 +5,37 @@
 #include <stdio.h>
 
 NCH_STATIC_INLINE void
-_generate_pieces_psudo_pawns(Board* board){
-    uint64 op_map, sqr;
-    uint64 pawns_map;
+generate_pieces_psudo_pawns(Board* board){
+    uint64 op_map;
     int idx;
     
     uint64 allowed_map = ~Board_ALL_OCC(board);
     if (Board_IS_WHITETURN(board)){
-        pawns_map = Board_WHITE_PAWNS(board);
         op_map = Board_BLACK_OCC(board) | board->en_passant_trg;
 
-        LOOP_U64_T(pawns_map){
-            sqr = NCH_SQR(idx);
-            board->moves[idx] = ((NCH_CHKUNI(sqr, NCH_ROW2) && NCH_CHKFLG(allowed_map, (0x010100ULL << idx))) ?
-                                 (0x010100ULL << idx) : (NCH_NXTSQR_UP(sqr) & allowed_map))
+        LOOP_U64_T(Board_WHITE_PAWNS(board)){
+            board->moves[idx] = (((NCH_GET_ROWIDX(idx) == 1) && NCH_CHKFLG(allowed_map, (0x010100ULL << idx))) ?
+                                 (0x010100ULL << idx) : (NCH_NXTSQR_UP(NCH_SQR(idx)) & allowed_map))
                                  | (bb_pawn_attacks(NCH_White, idx) & op_map);
         }
 
     }
     else{
-        pawns_map = Board_BLACK_PAWNS(board);
         op_map = Board_WHITE_OCC(board) | board->en_passant_trg;
 
-        LOOP_U64_T(pawns_map){
-            sqr = NCH_SQR(idx);
-            board->moves[idx] = ((NCH_CHKUNI(sqr, NCH_ROW7) && NCH_CHKFLG(allowed_map, 0x0080800000000000ULL >> (63 - idx))) ?
-                                 (0x0080800000000000ULL >> (63 - idx)): NCH_NXTSQR_DOWN(sqr) & allowed_map)
+        LOOP_U64_T(Board_BLACK_PAWNS(board)){
+            board->moves[idx] = (((NCH_GET_ROWIDX(idx) == 6) && NCH_CHKFLG(allowed_map, 0x0080800000000000ULL >> (63 - idx))) ?
+                                 (0x0080800000000000ULL >> (63 - idx)): NCH_NXTSQR_DOWN(NCH_SQR(idx)) & allowed_map)
                                  | (bb_pawn_attacks(NCH_Black, idx) & op_map);
         }
     }
 }
 
 NCH_STATIC_INLINE void
-_generate_pieces_psudo_king(Board* board){
+generate_pieces_psudo_king(Board* board){
     Side side = Board_GET_SIDE(board);
     uint64 king_sqr, op_king_sqr, allowed_squares;
+
     if (side == NCH_White){
         king_sqr = Board_WHITE_KING(board);
         op_king_sqr = Board_BLACK_KING(board);
@@ -67,7 +63,7 @@ _generate_pieces_psudo_king(Board* board){
 }
 
 NCH_STATIC_INLINE void
-_generate_pieces_psudo_others(Board* board){
+generate_pieces_psudo_others(Board* board){
     uint64 knight_map, bishop_map, rook_map, queen_map;
     uint64 allowed_squares;
     if (Board_IS_WHITETURN(board)){
@@ -109,7 +105,7 @@ _generate_pieces_psudo_others(Board* board){
 
 NCH_STATIC_INLINE void 
 generate_castle_moves(Board* board){
-    if (Board_IS_CHECK(board)){
+    if (Board_IS_CHECK(board) || !board->castles){
         return;
     }
 
@@ -129,14 +125,14 @@ generate_castle_moves(Board* board){
         }
     }
     else{
-        if (Board_IS_CASTLE_BK(board) && !NCH_CHKUNI(Board_ALL_OCC(board), 0x0600000000000000)
+        if (Board_IS_CASTLE_BK(board) && !NCH_CHKUNI(Board_ALL_OCC(board), (NCH_SQR(NCH_F8) | NCH_SQR(NCH_G8)))
             && !get_checkmap(board, NCH_Black, NCH_G8, Board_ALL_OCC(board)) 
             && !get_checkmap(board, NCH_Black, NCH_F8, Board_ALL_OCC(board))){
             
             board->castle_moves |= Board_CASTLE_BK;
         }
 
-        if (Board_IS_CASTLE_BQ(board) && !NCH_CHKUNI(Board_ALL_OCC(board), 0x7000000000000000)
+        if (Board_IS_CASTLE_BQ(board) && !NCH_CHKUNI(Board_ALL_OCC(board), (NCH_SQR(NCH_D8) | NCH_SQR(NCH_C8) | NCH_SQR(NCH_B8)))
             && !get_checkmap(board, NCH_Black, NCH_D8, Board_ALL_OCC(board)) 
             && !get_checkmap(board, NCH_Black, NCH_C8, Board_ALL_OCC(board))){
             
@@ -196,8 +192,8 @@ generate_moves(Board* board){
 
     uint64 allowed_squares = get_allowed_squares(board);
     if (allowed_squares){
-        _generate_pieces_psudo_pawns(board);
-        _generate_pieces_psudo_others(board);
+        generate_pieces_psudo_pawns(board);
+        generate_pieces_psudo_others(board);
 
         if (allowed_squares != NCH_UINT64_MAX){
             for (int i = 0; i < NCH_SQUARE_NB; i++){
@@ -214,9 +210,8 @@ generate_moves(Board* board){
         }
 
         execlude_pinned_pieces_unlegal_moves(board);
+        generate_castle_moves(board);
     }
 
-
-    _generate_pieces_psudo_king(board);
-    generate_castle_moves(board);
+    generate_pieces_psudo_king(board);
 }

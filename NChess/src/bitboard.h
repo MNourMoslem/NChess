5c380654546c3d331035a46ae643042b93246af0
= <normoslem256@gmail.com>
2025-01-10 01:36:44 +0300

    refactor(bitboard): replace NCH_STATIC_INLINE with NCH_STATIC_FINLINE for performance optimization

--- NChess/src/bitboard.h
+++ NChess/src/bitboard.h
@@ -23,57 +23,57 @@ extern uint64 SlidersAttackMask[2][NCH_SQUARE_NB];         // 128
 extern uint64 RookTable[NCH_SQUARE_NB][4096];              // 262,144
 extern uint64 BishopTable[NCH_SQUARE_NB][512];             // 32,768
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_between(int from_, int to_){
     return BetweenTable[from_][to_];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_pawn_attacks(Side side, int sqr_idx){
     return PawnAttacks[side][sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_knight_attacks(int sqr_idx){
     return KnightAttacks[sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_king_attacks(int sqr_idx){
     return KingAttacks[sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_rook_mask(int sqr_idx){
     return SlidersAttackMask[NCH_RS][sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_bishop_mask(int sqr_idx){
     return SlidersAttackMask[NCH_BS][sqr_idx];
 }
 
-NCH_STATIC_INLINE int
+NCH_STATIC_FINLINE int
 bb_rook_relevant(int sqr_idx){
     return ReleventSquares[NCH_RS][sqr_idx];
 }
 
-NCH_STATIC_INLINE int
+NCH_STATIC_FINLINE int
 bb_bishop_relevant(int sqr_idx){
     return ReleventSquares[NCH_BS][sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_rook_magic(int sqr_idx){
     return Magics[NCH_RS][sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_bishop_magic(int sqr_idx){
     return Magics[NCH_BS][sqr_idx];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_rook_attacks(int sqr_idx, uint64 block){
     block &= bb_rook_mask(sqr_idx);
     block *= bb_rook_magic(sqr_idx);
@@ -81,7 +81,7 @@ bb_rook_attacks(int sqr_idx, uint64 block){
     return RookTable[sqr_idx][block];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_bishop_attacks(int sqr_idx, uint64 block){
     block &= bb_bishop_mask(sqr_idx);
     block *= bb_bishop_magic(sqr_idx);
@@ -89,7 +89,7 @@ bb_bishop_attacks(int sqr_idx, uint64 block){
     return BishopTable[sqr_idx][block];
 }
 
-NCH_STATIC_INLINE uint64
+NCH_STATIC_FINLINE uint64
 bb_queen_attacks(int sqr_idx, uint64 block){
     return bb_rook_attacks(sqr_idx, block) | bb_bishop_attacks(sqr_idx, block);
 }

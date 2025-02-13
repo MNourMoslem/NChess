bc2fbbfaffa6c714ce63c23b3636a9eb72baa542
MNourMoslem <normoslem256@gmail.com>
2025-02-13 10:35:15 +0300

    refactor(fen): enhance documentation for fen.h and clarify future changes

--- c-nchess/nchess/fen.h
+++ c-nchess/nchess/fen.h
@@ -1,3 +1,14 @@
+/*
+    fen.h
+
+    This file contains the function to create a board from a fen string.
+    In the future a Board_AsFen would be added to this file to convert the board
+    to a fen string.
+
+    The way Board_FromFen behaves would change in the future to be able to create
+    the board on the stack and without the need to allocate memory for the board.
+*/
+
 #ifndef NCHESS_SRC_FEN_H
 #define NCHESS_SRC_FEN_H
 
@@ -6,6 +17,7 @@
 #include "types.h"
 #include "config.h"
 
+// creates a board from a fen string
 Board*
 Board_FromFen(char* fen);
 

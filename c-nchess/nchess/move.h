eb1123a26fd89291cc5f669923f92db66cce54ef
= <normoslem256@gmail.com>
2025-02-24 12:43:19 +0300

    feat: change the en passant enum from MoveType_Enpassant to MoveType_EnPassant and added doucmentation to the file

--- c-nchess/nchess/move.h
+++ c-nchess/nchess/move.h
@@ -1,3 +1,10 @@
+/*
+    move.h
+
+    This file contains the typedef of Move. It also contains function
+    declarations related to Move, such as creating and printing moves, etc.
+*/
+
 #ifndef NCHESS_SRC_MOVE_H
 #define NCHESS_SRC_MOVE_H
 
@@ -5,32 +12,14 @@
 #include "types.h"
 #include "config.h"
 
-typedef enum{
+typedef enum {
     MoveType_Normal,
     MoveType_Promotion,
-    MoveType_Enpassant,
+    MoveType_EnPassant,
     MoveType_Castle,
-}MoveType;
+} MoveType;
 
 typedef uint16 Move;
-
-#define _Move_New(from_, to_, promotion_piece, move_type)\
-Move_ASSIGN_FROM(from_)\
-| Move_ASSIGN_TO(to_)\
-| Move_ASSIGN_PRO_PIECE(promotion_piece - 1)\
-| Move_ASSIGN_TYPE(move_type)
-
-Move
-Move_FromString(const char* move_str);
-
-void
-Move_Print(Move move);
-
-int
-Move_AsString(Move move, char* dst);
-
-void
-Move_PrintAll(Move* move, int nmoves);
 
 #define Move_ASSIGN_FROM(from_) ((from_))
 #define Move_ASSIGN_TO(to_) ((to_) << 6)
@@ -42,4 +31,40 @@ Move_PrintAll(Move* move, int nmoves);
 #define Move_PRO_PIECE(move) ((((move) >> 12) & 0x3) + 1)
 #define Move_TYPE(move) (((move) >> 14) & 0x3)
 
-#endif //NCHESS_SRC_MOVE_H
+// A macro to create a Move. It is faster but not safe
+// if the given parameters are incorrect. Use Move_New for safer usage.
+#define _Move_New(from_, to_, promotion_piece, move_type) \
+    Move_ASSIGN_FROM(from_) | \
+    Move_ASSIGN_TO(to_) | \
+    Move_ASSIGN_PRO_PIECE(promotion_piece - 1) | \
+    Move_ASSIGN_TYPE(move_type)
+
+
+// Returns a new move if it is valid, and 0 if not.
+Move 
+Move_New(Square from_, Square to_, MoveType type, Piece promotion_piece);
+
+// Returns a move from a UCI string. The move type is MoveType_Normal
+// by default unless it is a promotion move.
+// In other words, MoveType_Castle and MoveType_EnPassant cannot be
+// detected, and it is not the responsibility of this function.
+// Also if promotion piece is not a valid char (q, r, b, k) the piece 
+// would be set to NCH_Queen by default.
+// Returns 0 if the move is not valid.
+Move 
+Move_FromString(const char* move_str);
+
+// Prints a move to the console.
+void 
+Move_Print(Move move);
+
+// Converts a Move to a UCI string.
+// Returns 0 on success and -1 on failure.
+int 
+Move_AsString(Move move, char* dst);
+
+// Prints all moves in a given buffer.
+void 
+Move_PrintAll(Move* move, int nmoves);
+
+#endif // NCHESS_SRC_MOVE_H

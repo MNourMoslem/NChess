5a6da45d4c738a6bacd208510cb93cc15bcf361b
= <normoslem256@gmail.com>
2025-03-01 10:15:37 +0300

    feat: added bb_new and moved methods to pybb_methods.

--- NChess/core/src/PyBB.c
+++ NChess/core/src/PyBB.c
@@ -2,9 +2,7 @@
 #include "pybb.h"
 #include "array_conversion.h"
 #include "common.h"
-
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <numpy/arrayobject.h>
+#include "pybb_methods.h"
 
 #define PY_SSIZE_CLEAN_T
 #include <Python.h>
@@ -12,208 +10,27 @@
 PyBitBoard* PyBitBoard_FromUnsignedLongLong(unsigned long long value){
     PyObject* obj = PyLong_FromUnsignedLongLong(value);
     if (!obj){
-        PyErr_SetString(
-            PyExc_ValueError,
-            "Falied to create a bitboard object"
-        );
+        if (!PyErr_Occurred()){
+            PyErr_SetString(
+                PyExc_ValueError,
+                "Falied to create a bitboard object"
+            );
+        }
         return NULL;
     }
     obj->ob_type = &PyBitBoardType;
-    return obj;
+    return (PyObject*)obj;
 }
 
 NCH_STATIC PyObject*
-bb_as_array(PyObject* self, PyObject* args, PyObject* kwargs){
-    npy_intp nitems = NCH_SQUARE_NB;
-    npy_intp dims[NPY_MAXDIMS];
-    int reversed, as_list;
-    int ndim = parse_array_conversion_function_args(nitems, dims, args, kwargs, &reversed, &as_list);
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
+bb_new(PyTypeObject* type, PyObject* args, PyObject* kwargs) {
+    unsigned long long value;    
+    if (!PyArg_ParseTuple(args, "L", &value)) {
         return NULL;
     }
 
-    if (ndim < 0)
-        return NULL;
-
-    if (!ndim){
-        ndim = 1;
-        dims[0] = nitems;
-    }
-
-    if (as_list){
-        int data[NCH_SQUARE_NB];
-        bb2array(bb, data, reversed);
-        return create_list_array(data, dims, ndim);
-    }
-
-    int* data = (int*)malloc(nitems * sizeof(int));
-    if (!data){
-        PyErr_NoMemory();
-        return NULL;
-    }
-
-    bb2array(bb, data, reversed);
-    
-    PyObject* array = create_numpy_array(data, dims, ndim, NPY_INT);
-    if (!array){
-        free(data);
-        if (!PyErr_Occurred()){
-            PyErr_SetString(PyExc_RuntimeError, "Failed to create array");
-        }
-        return NULL;
-    }
-
-    return array;
+    return PyLong_Type.tp_new(type, args, kwargs);
 }
-
-NCH_STATIC PyObject*
-bb_more_then_one(PyObject* self, PyObject* args){
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-    return PyBool_FromLong(more_then_one(bb));
-}
-
-NCH_STATIC PyObject*
-bb_has_two_bits(PyObject* self, PyObject* args){
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-    return PyBool_FromLong(has_two_bits(bb));
-}
-
-NCH_STATIC PyObject*
-bb_get_last_bit(PyObject* self, PyObject* args) {
-    uint64_t bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-    uint64_t last_bit = get_last_bit(bb);
-    return (PyObject*)PyBitBoard_FromUnsignedLongLong(last_bit);
-}
-
-NCH_STATIC PyObject*
-bb_count_bits(PyObject* self, PyObject* args){
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-
-    return PyLong_FromLong(count_bits(bb));
-}
-
-NCH_STATIC PyObject*
-bb_is_filled(PyObject* self, PyObject* args, PyObject* kwargs){
-    PyObject* sqr;
-    static char* kwlist[] = {"square", NULL};
-    
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O", kwlist, &sqr)){
-        if (!PyErr_Occurred()){
-            PyErr_SetString(PyExc_ValueError, "failed to parse the arguments");
-        }
-        return NULL;
-    }
-    
-    Square s = pyobject_as_square(sqr);
-    if (s == NCH_NO_SQR)
-        Py_RETURN_NONE;
-    
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-    return PyBool_FromLong(bb & NCH_SQR(s));
-}
-
-NCH_STATIC PyObject* 
-bb_to_squares(PyObject* self, PyObject* args){
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-
-    PyObject* list = PyList_New(count_bits(bb));
-    if (!list)
-        return NULL;
-
-    int idx;
-    Py_ssize_t i = 0;
-    LOOP_U64_T(bb){
-        PyList_SetItem(list, i++, PyLong_FromLong(idx));
-    }
-
-    return list;
-}
-
-NCH_STATIC PyObject*
-bb_set_square(PyObject* self, PyObject* args, PyObject* kwargs){
-    PyObject* sqr;
-    static char* kwlist[] = {"square", NULL};
-    
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O", kwlist, &sqr)){
-        if (!PyErr_Occurred()){
-            PyErr_SetString(PyExc_ValueError, "failed to parse the arguments");
-        }
-        return NULL;
-    }
-    
-    Square s = pyobject_as_square(sqr);
-    if (s == NCH_NO_SQR){
-        if (!PyErr_Occurred()){
-            PyErr_SetString(PyExc_ValueError, "failed to convert input to square");
-        }
-        return NULL;
-    }
-    
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-    return (PyObject*)PyBitBoard_FromUnsignedLongLong(bb | NCH_SQR(s));
-}
-
-NCH_STATIC PyObject*
-bb_remove_square(PyObject* self, PyObject* args, PyObject* kwargs){
-    PyObject* sqr;
-    static char* kwlist[] = {"square", NULL};
-    
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O", kwlist, &sqr)){
-        if (!PyErr_Occurred()){
-            PyErr_SetString(PyExc_ValueError, "failed to parse the arguments");
-        }
-        return NULL;
-    }
-    
-    Square s = pyobject_as_square(sqr);
-    if (s == NCH_NO_SQR){
-        if (!PyErr_Occurred()){
-            PyErr_SetString(PyExc_ValueError, "failed to convert input to square");
-        }
-        return NULL;
-    }
-    
-    uint64 bb = PyLong_AsUnsignedLongLong(self);
-    if (PyErr_Occurred()) {
-        return NULL;
-    }
-    return (PyObject*)PyBitBoard_FromUnsignedLongLong(bb & ~NCH_SQR(s));
-}
-
-PyMethodDef bitboard_methods[] = {
-    {"as_array"     , (PyCFunction)bb_as_array     , METH_VARARGS | METH_KEYWORDS, NULL},
-    {"more_then_one", (PyCFunction)bb_more_then_one, METH_NOARGS                 , NULL},
-    {"has_two_bits" , (PyCFunction)bb_has_two_bits , METH_NOARGS                 , NULL},
-    {"get_last_bit" , (PyCFunction)bb_get_last_bit , METH_NOARGS                 , NULL},
-    {"count_bits"   , (PyCFunction)bb_count_bits   , METH_NOARGS                 , NULL},
-    {"is_filled"    , (PyCFunction)bb_is_filled    , METH_VARARGS | METH_KEYWORDS, NULL},
-    {"to_squares"   , (PyCFunction)bb_to_squares   , METH_NOARGS                 , NULL},
-    {"set_square"   , (PyCFunction)bb_set_square   , METH_VARARGS | METH_KEYWORDS, NULL},
-    {"remove_square", (PyCFunction)bb_remove_square, METH_VARARGS | METH_KEYWORDS, NULL},
-    {NULL           , NULL                         , 0                           , NULL},
-};
 
 NCH_STATIC PyObject*
 bb_iter(PyObject* self){
@@ -239,19 +56,24 @@ bb_iter(PyObject* self){
 
 static PyObject*
 bb_str(PyBitBoard* self) {
+    unsigned long long value = PyLong_AsUnsignedLongLong((PyObject*)self);
+    if (PyErr_Occurred()) {
+        return NULL;
+    }
+
     PyObject* class_name_obj = PyObject_GetAttrString((PyObject*)Py_TYPE(self), "__name__");
     if (!class_name_obj) {
         return PyUnicode_FromString("<UnknownClass>");
     }
 
-    unsigned long long value = PyLong_AsUnsignedLongLong((PyObject*)self);
-    if (PyErr_Occurred()) {
-        Py_DECREF(class_name_obj);
-        return NULL;
-    }
+    // Use C snprintf() to format the hexadecimal representation
+    char hex_buffer[20];  // Enough for "0x" + 16 hex digits + null terminator
+    snprintf(hex_buffer, sizeof(hex_buffer), "0x%llx", value);
 
-    PyObject* result = PyUnicode_FromFormat("%U(0x%x)", class_name_obj, value);
+    // Create the final Python string
+    PyObject* result = PyUnicode_FromFormat("%U(%s)", class_name_obj, hex_buffer);
     Py_DECREF(class_name_obj);
+    
     return result;
 }
 
@@ -264,6 +86,7 @@ PyTypeObject PyBitBoardType = {
     .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
     .tp_str = (reprfunc)bb_str,
     .tp_repr = (reprfunc)bb_str,
-    .tp_methods = bitboard_methods,
+    .tp_methods = pybb_methods,
     .tp_iter = bb_iter,
+    .tp_new = bb_new,
 };

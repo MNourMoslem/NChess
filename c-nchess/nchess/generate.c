c3ee3a4afdcbf55a5db2f772526e46dcd4dc9ca1
= <normoslem256@gmail.com>
2025-03-01 10:13:35 +0300

    fix: update piece handling in Board_GeneratePseudoMovesOf function

--- c-nchess/nchess/generate.c
+++ c-nchess/nchess/generate.c
@@ -377,17 +377,19 @@ Board_GeneratePseudoMovesOf(Board* board, Move* moves, Square sqr){
     if (!is_valid_square(sqr))
         return 0;
 
-    PieceType p = Piece_TYPE(Board_PIECE(board, sqr));
-    if (p == NCH_NO_PIECE_TYPE)
+    Piece p = Board_PIECE(board, sqr);
+    if (p == NCH_NO_PIECE)
         return 0;
 
+    PieceType pt = Piece_TYPE(p);
+
     Move* begin = moves;
-    if (p == NCH_King){
+    if (pt == NCH_King){
         moves = generate_castle_moves(board, moves);
         moves = generate_king_moves(board, moves);
     }
     else{
-        uint64 allowed_square = ~Board_OCC(board, Board_SIDE(board));
+        uint64 allowed_square = ~Board_OCC(board, Piece_SIDE(p));
         moves = generate_any_move(board, sqr, allowed_square, moves);
     }
 

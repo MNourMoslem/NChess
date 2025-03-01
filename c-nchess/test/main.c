55c0b36710f7e2593f8d6d3c860171e43532c1a1
= <normoslem256@gmail.com>
2025-03-01 16:27:18 +0300

    feat and fix: added test_fen.c and update test main function to initialize variables and include new test cases

--- c-nchess/test/main.c
+++ c-nchess/test/main.c
@@ -2,16 +2,19 @@
 #include "test_io.c"
 #include "test_bitboards.c"
 #include "test_perft.c"
+#include "test_fen.c"
 
 #include "nchess.h"
 
 int main(){
     NCH_Init();
+    int init = 0;
 
     // test_io_main(0);
-    test_bitboard_main(0);
-    test_perft_main(0, 1);
-    test_copy_main(0);
+    test_bitboard_main(init);
+    test_perft_main(init, 1);
+    test_copy_main(init);
+    test_fen_main(init);
 
     return 0;
 }
